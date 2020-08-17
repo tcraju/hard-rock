6cc592dec5fda022afe853f2571be57302586ad0
tcraju <tcnajmul@gmail.com>
2020-08-17 16:42:25 +0600

    2nd commit

--- app.js
+++ app.js
@@ -14,11 +14,11 @@ function songSearch() {
 
                 for (var i = 0; i < 10; i++) {
                     songList.innerHTML += `<p class="author lead"><strong id= 'song-title-${i}'>${json.data[i].title}</strong> Album by <span>${json.data[i].album.title}</span> <button class="btn btn-success" onclick="lyricSearch(${i})">Get Lyrics</button></p>
-                                            <p class="d-none" id='artist-name-${i}'>${json.data[i].artist.name}</p>`
+                                            <p class="d-none" id='artist-name-${i}'>${json.data[i].artist.name}</p>`;
                 }
-            }
+            };
             fetchSongItem();
-        })
+        });
 }
 
 
@@ -32,5 +32,9 @@ const lyricSearch = (i) => {
         .then(json => {
             document.getElementById('lyric-title').innerText = songTitle.innerText;
             document.querySelector('.lyric').innerText = json.lyrics;
-        })
-}
+        });
+
+
+
+        
+};
